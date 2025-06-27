842b6e9b614ba07fb22d15b53bf4083af9d12158
fdebailleul <166001368+fdebailleul@users.noreply.github.com>
2025-06-27 09:15:36 +0200

    Apply new UI design

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import QRCodeGenerator from './components/QRCodeGenerator';
 
 function App() {
   return (
-    <div className="App">
-      <QRCodeGenerator />
-    </div>
+    <>
+      <header>
+        <div className="logo">QRGen</div>
+        <nav>
+          <a href="#">Product</a>
+          <a href="#">Resources</a>
+          <a href="#">Support</a>
+          <a href="#">About</a>
+          <a href="#">Blog</a>
+        </nav>
+        <div className="auth">
+          <a href="#" className="signin">Sign in</a>
+          <a href="#" className="signup">Sign up</a>
+        </div>
+      </header>
+
+      <main>
+        <QRCodeGenerator />
+      </main>
+
+      <footer>
+        <a href="#">Help Center</a> &bull;{' '}
+        <a href="#">Privacy</a> &bull;{' '}
+        <a href="#">Terms</a>
+        <div>&copy; QRGen Co.</div>
+      </footer>
+    </>
   );
 }
 
